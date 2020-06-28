b192f71d9400b3bbf28a472edab267db40acd00d
SE130058\Some else <giabaohoang9199@gmail.com>
2020-06-28 22:54:18 +0700

    Add database and generate HTML file to check wellform

--- Console/Wellformer/src/wellformer/Wellformer.java
+++ Console/Wellformer/src/wellformer/Wellformer.java
@@ -5,8 +5,20 @@
  */
 package wellformer;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import prx.utils.HttpUtils;
+import prx.utils.TextUtils;
 
 /**
  *
@@ -18,22 +30,65 @@ public class Wellformer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO TEST SyntaxWellformer
-        SyntaxWellformer wellformer = new SyntaxWellformer();
-        Map<String,String> map  =new LinkedHashMap<>();
-        map.put("Attribute không value", "<h1 checked> YEAH</h1>");
-        map.put("Value không bọc trong cặp nháy", "<h1 aa= aa> <img a=a />YEAH</h1>");
-        map.put("Attribute dính liền nhau", "<h1 a=\"1\"b='2'c=3>YEAH</h1>");
-        map.put("Empty element", "<h1><img src=\"\"><br><hr/></h1>");
-        map.put("Lỗi đóng mở thẻ", "<li><a>Sach moi</a></h3>");
-        
-        for(Map.Entry<String, String> entry: map.entrySet()){
-            System.out.println(entry.getKey());
-            
-            System.out.println(entry.getValue());
-            System.out.println(wellformer.check(entry.getValue()));
-            System.out.println();
+        List<String> urlList = new ArrayList();
+        urlList.add("https://www.coursera.org/browse/computer-science");
+        String pageContent = null;
+        try {
+            for (String page : urlList) {
+                pageContent = HttpUtils.getContent(page);
+                // test output
+                // [BHG] TODO Delete on finish project
+                PrintWriter pw = new PrintWriter("rawPageContent.html");
+                pw.print(pageContent);
+                pw.close();
+                pageContent = TextUtils.refineHtml(pageContent);
+                pw = new PrintWriter("refinedPageContent.html");
+                pw.print(pageContent);
+                pw.close();
+                boolean isWellformed = checkWellformXML(pageContent);
+                if(isWellformed){
+                    System.out.println(page + " đã well-form");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
-    
+
+    public static boolean checkWellformXML(String src) {
+        // use DocumentBuilder + SAX parser to check 
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setValidating(false);
+        factory.setNamespaceAware(true);
+
+        DocumentBuilder builder;
+        try {
+            builder = factory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+            return false;
+        }
+        builder.setErrorHandler(new ErrorHandler() {
+            @Override
+            public void warning(SAXParseException exception) throws SAXException {
+                System.out.println(exception.getMessage());
+            }
+
+            @Override
+            public void error(SAXParseException exception) throws SAXException {
+                System.out.println(exception.getMessage());
+            }
+
+            @Override
+            public void fatalError(SAXParseException exception) throws SAXException {
+                System.out.println(exception.getMessage());
+            }
+        });
+        try {
+            builder.parse(new ByteArrayInputStream(src.getBytes(StandardCharsets.UTF_8)));
+            return true;
+        } catch (SAXException | IOException e) {
+            return false;
+        }
+    }
 }
