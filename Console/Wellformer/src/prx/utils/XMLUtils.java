2d2e55f2fd09b1eddc06e8178ec302d9ad757bf4
SE130058\Some else <giabaohoang9199@gmail.com>
2020-06-29 14:27:35 +0700

    XMLUtils init

--- Console/Wellformer/src/prx/utils/XMLUtils.java
+++ Console/Wellformer/src/prx/utils/XMLUtils.java
@@ -5,10 +5,47 @@
  */
 package prx.utils;
 
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
 /**
  *
  * @author Gia Bảo Hoàng
  */
 public class XMLUtils {
+    public static Document parseFileToDOM(String xmlFilePath) throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(xmlFilePath);
+        
+        return doc;
+    }
     
+    public static XPath getXPath(){
+        XPathFactory factory = XPathFactory.newInstance();
+        XPath xpath = factory.newXPath();
+        return xpath;
+    }
+    
+    public static void TransformXMLToFile(Node node, String xmlFilePath) throws TransformerException{
+        TransformerFactory factory = TransformerFactory.newInstance();
+        Transformer transformer = factory.newTransformer();
+        Source src = new DOMSource(node);
+        Result result = new StreamResult(xmlFilePath);
+        transformer.transform(src, result);
+    }
 }
